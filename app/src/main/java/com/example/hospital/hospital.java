eec8d6a8ff79a36fb26d52ba2643061e6fd4ae94
Leeyujin <switch2001@naver.com>
2023-06-20 06:01:52 +0900

    병원 open api 연동 실패

---
+++ app/src/main/java/com/example/hospital/hospital.java
@@ -0,0 +1,17 @@
+package com.example.hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class hospital extends AppCompatActivity {
+    public String 총면적;
+    public String 전문과목;
+    public String 의료기관명;
+    public String 의료기관주소;
+    public String 의료기관전화번호;
+    public int 순번;
+    public int 입원실;
+    public int 병상;
+
+
+
+}
